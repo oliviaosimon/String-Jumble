411968146b7845cc9c46906c40d02c0e830bddf8
oliviaosimon <42871888+oliviaosimon@users.noreply.github.com>
2018-09-30 19:37:59 -0400

    Update stringjumble.py

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
-Author: <your name>
-Credit: <sources>
+Author: Olivia Simon
+Credit: https://stackoverflow.com/questions/931092/reverse-a-string-in-python
 
 Assignment:
 
@@ -25,3 +25,12 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+text = input("Please enter a string of text (the bigger the better): ")
+print("You entered "+text+". Now jumble it:")
+wordSplit =text.split()
+letterSplit=list(text)
+
+#text jumbles
+print(text [::-1])
+print(wordSplit [::-1])
+print(" ".join(word[::-1] for word in wordSplit))
